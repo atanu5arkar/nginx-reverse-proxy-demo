2395fc932889e26bc757a606b40180582484818f
atanu5arkar <atanu_sarkar1@outlook.com>
2024-07-23 08:47:34 +0530

    adds another route

--- app.js
+++ app.js
@@ -7,6 +7,10 @@ app.get("/", (req, res) => {
     res.status(200).send(`<h2>This is Express from port ${port}</h2>`);
 })
 
+app.get("/hello", (req, res) => {
+    res.status(200).send(`Hello from the Hello route`);
+})
+
 app.listen(port, () => {
     console.log("Server running at", port);
 })
